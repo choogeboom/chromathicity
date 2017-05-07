c82bc6be4d2897652e056676d03650d53a160509
hoogamaphone <chris.hoogeboom@gmail.com>
2017-05-06 23:46:47 -0400

    added some custom exceptions

--- chromathicity/error.py
+++ chromathicity/error.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 
 def raise_not_implemented(obj, message: str):
     """
@@ -20,6 +22,17 @@ class ChromathicityError(Exception):
 
 class UndefinedConversionError(ChromathicityError):
     """ Raised when a conversion is undefined """
-    pass
+    def __init__(self, from_space: str, to_space: str):
+        self.message = f'Conversion from {from_space} to ' \
+                       f'{to_space} is undefined.'
 
+
+class UndefinedColorSpaceError(ChromathicityError):
+    """ Raised when a color space is not recognized """
+    def __init__(self, space: Union[str, type]):
+        if isinstance(space, str):
+            self.message = f'Color space {space} is unrecognized.'
+        elif isinstance(space, type):
+            self.message = f'Class {space.__name__} is not a recognized ' \
+                           f'color space. Use the color_space decorator.'
 
