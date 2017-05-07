ef280a0c2a39d8a5ff3afb7fb9ca056cd3e32588
hoogamaphone <chris.hoogeboom@gmail.com>
2017-05-06 23:49:18 -0400

    major overhaul, adding functionality, and cleaning up the code.

--- chromathicity/spaces.py
+++ chromathicity/spaces.py
@@ -1,13 +1,16 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from copy import copy
 import sys
 from typing import Union
 
 from bidict import bidict
 import numpy as np
 
-from chromathicity.convert import xyz2xyz, convert, get_matching_axis
+from chromathicity.convert import xyz2xyz, convert, get_matching_axis, \
+    construct_component_inds
 from chromathicity.chromadapt import (
     get_default_chromatic_adaptation_algorithm, ChromaticAdaptationAlgorithm)
+from chromathicity.error import raise_not_implemented, UndefinedColorSpaceError
 from chromathicity.illuminant import get_default_illuminant, Illuminant
 from chromathicity.rgbspec import (get_default_rgb_specification,
                                    RgbSpecification)
@@ -15,6 +18,46 @@ from chromathicity.observer import get_default_observer, Observer
 
 
 _space_name_map = bidict()
+
+
+def get_space(space: Union[str, type]):
+    """ Get the space name and class associated with it"""
+    if isinstance(space, str):
+        if space in _space_name_map:
+            space_name = space
+            space_class_name = _space_name_map[space]
+        elif space in _space_name_map.inv:
+            space_name = _space_name_map.inv[space]
+            space_class_name = space
+        else:
+            raise UndefinedColorSpaceError(space)
+        space_class = getattr(sys.modules[__name__], space_class_name)
+    elif isinstance(space, type) and issubclass(space, ColorSpaceData):
+        if space.__name__ in _space_name_map.inv:
+            space_name = _space_name_map.inv[space.__name__]
+            space_class = space
+        else:
+            raise UndefinedColorSpaceError(space)
+    else:
+        raise TypeError(f'Illegal color space type: {type(space).__name__}')
+    return space_name, space_class
+
+
+def get_space_type(space_name: str):
+    """Get the color space type associated with a color space"""
+    if space_name in _space_name_map:
+        class_name = _space_name_map[space_name]
+        return getattr(sys.modules[__name__], class_name)
+    else:
+        raise UndefinedColorSpaceError(space_name)
+
+
+def get_space_name(space_type: type):
+    """Get the color space name associated with a color space"""
+    if space_type.__name__ in _space_name_map.inv:
+        return _space_name_map.inv[space_type.__name__]
+    else:
+        raise UndefinedColorSpaceError(space_type)
 
 
 def color_space(name: str):
@@ -31,47 +74,109 @@ def color_space(name: str):
     return decorator
 
 
-class ColorSpaceDataBase(ABC):
-
+class ColorSpaceData(ABC):
+    """
+    Defines the main interface for all color space data classes
+    """
     __spacename__ = ''
 
+    @property
+    @abstractmethod
+    def data(self):
+        pass
+
+    @property
+    @abstractmethod
+    def axis(self):
+        pass
+
+    @property
+    @abstractmethod
+    def illuminant(self):
+        pass
+
+    @illuminant.setter
+    def illuminant(self, ill: Illuminant):
+        raise_not_implemented(self, 'setting illuminant')
+
+    @property
+    @abstractmethod
+    def observer(self):
+        pass
+
+    @observer.setter
+    def observer(self, obs: Observer):
+        raise_not_implemented(self, 'setting observer')
+
+    @property
+    @abstractmethod
+    def rgbs(self):
+        pass
+
+    @rgbs.setter
+    def rgbs(self, r: RgbSpecification):
+        raise_not_implemented(self, 'setting rgbs')
+
+    @property
+    @abstractmethod
+    def caa(self):
+        pass
+
+    @caa.setter
+    def caa(self, c):
+        raise_not_implemented(self, 'setting caa')
+
+    @abstractmethod
+    def to(self, space: Union[str, type]):
+        pass
+
+
+class ColorSpaceDataImpl(ColorSpaceData):
+    """
+    A full implementation of the ColorSpaceDataBase interface. All color space 
+    data classes should inherit this.
+    """
     def __init__(self,
-                 data: np.ndarray,
+                 data: Union[np.ndarray, ColorSpaceData],
                  axis: int=None,
                  illuminant: Illuminant=None,
                  observer: Observer=None,
                  rgbs: RgbSpecification=None,
                  caa: ChromaticAdaptationAlgorithm=None):
-        if isinstance(data, ColorSpaceDataBase):
-            self._axis = data._axis
-            self._illuminant = data._illuminant
-            self._observer = data._observer
-            self._rgbs = data._rgbs
-            self._caa = data._caa
-            data = data._data
+        """
+        
+        :param data: the color space data to contain
+        :param axis: the axis along which the color data lies
+        :param illuminant: the illuminant
+        :param observer: the observer
+        :param rgbs: the rgb specification
+        :param caa: the chromatic adaptation algorithm
+        """
+        if isinstance(data, ColorSpaceData):
+            self._axis = data.axis
+            self._illuminant = data.illuminant
+            self._observer = data.observer
+            self._rgbs = data.rgbs
+            self._caa = data.caa
+            data = data.data
 
         self._data = np.array(data, copy=True)
         self._data.flags.writeable = False
-        if axis is None:
-            self._axis = get_matching_axis(self._data.shape, 3)
-        else:
-            self._axis = axis
-        if illuminant is None:
-            self._illuminant = get_default_illuminant()
-        else:
-            self._illuminant = illuminant
-        if observer is None:
-            self._observer = get_default_observer()
-        else:
-            self._observer = observer
-        if rgbs is None:
-            self._rgbs = get_default_rgb_specification()
-        else:
-            self._rgbs = rgbs
-        if caa is None:
-            self._caa = get_default_chromatic_adaptation_algorithm()
-        else:
-            self._caa = caa
+        self._axis = (axis
+                      if axis is not None
+                      else get_matching_axis(self._data.shape, 3))
+        self._illuminant = (illuminant
+                            if illuminant is not None
+                            else get_default_illuminant())
+        self._observer = (observer
+                          if observer is not None
+                          else get_default_observer())
+        self._rgbs = (rgbs
+                      if rgbs is not None
+                      else get_default_rgb_specification())
+        self._caa = (caa
+                     if caa is not None
+                     else get_default_chromatic_adaptation_algorithm())
 
     def __array__(self, dtype):
         if dtype == self._data.dtype:
@@ -79,44 +184,52 @@ class ColorSpaceDataBase(ABC):
         else:
             return np.array(self._data, copy=True, dtype=dtype)
 
-    def to(self, space: Union[str, type]):
-        if isinstance(space, str):
-            if space in _space_name_map:
-                to_space = space
-                to_class_name = _space_name_map[space]
-            elif space in _space_name_map.inv:
-                to_space = _space_name_map.inv[space]
-                to_class_name = space
-            else:
-                raise ValueError(f'Illegal color space: {space}')
-            to_class = getattr(sys.modules[__name__], to_class_name)
-        elif isinstance(space, type):
-            if issubclass(space, ColorSpaceDataBase):
-                to_space = _space_name_map.inv[space.__name__]
-                to_class = space
-            else:
-                raise ValueError(f'Illegal color space type: {space.__name__}')
-        else:
-            raise TypeError(f'Illegal color space type: {type(space).__name__}')
-        from_space = _space_name_map.inv[type(self).__name__]
+    def __getitem__(self, *args, **kwargs):
+        return self.data.__getitem__(*args, **kwargs)
+
+    def __repr__(self):
+        kwarg_repr = ', '.join(f'{key!s}={value!r}'
+                               for key, value in self._get_kwargs().items())
+        return f'{type(self).__name__}({self.data!r}, {kwarg_repr}'
+
+    @property
+    def components(self):
+        component_inds = construct_component_inds(self.axis, self.data.ndim, 3)
+        return tuple(self[c] for c in component_inds)
+
+    def to(self, space: Union[str, type]) -> ColorSpaceData:
+        """
+        Convert this space to another
+        
+        :param space: either the name of the space, or the class
+        :return: The new color space data
+        """
+        to_space, to_class = get_space(space)
+        from_space = get_space_name(type(self))
         if from_space == to_space:
-            return self
-        converted_data = convert(self._data,
+            return copy(self)
+        kwargs = self._get_kwargs()
+        converted_data = convert(self.data,
                                  from_space=from_space,
                                  to_space=to_space,
-                                 illuminant=self._illuminant,
-                                 observer=self._observer,
-                                 rgbs=self._rgbs,
-                                 caa=self._caa)
-        return self._construct_space(to_class, converted_data)
+                                 **kwargs)
+        return to_class(data=converted_data,
+                        **kwargs)
 
-    def _construct_space(self, space, new_data):
-        return space(data=new_data,
-                     axis=self._axis,
-                     illuminant=self._illuminant,
-                     observer=self._observer,
-                     rgbs=self._rgbs,
-                     caa=self._caa)
+    def _get_kwargs(self):
+        return {'axis': self.axis,
+                'illuminant': self.illuminant,
+                'observer': self.observer,
+                'rgbs': self.rgbs,
+                'caa': self.caa}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def axis(self):
+        return self._axis
 
     @property
     def illuminant(self):
@@ -152,37 +265,51 @@ class ColorSpaceDataBase(ABC):
 
 
 @color_space('spectrum')
-class SpectralData(ColorSpaceDataBase):
-
-    def __init__(self, data, wavelengths: np.ndarray, axis, *args, **kwargs):
+class SpectralData(ColorSpaceDataImpl):
+    """ Contains raw reflectance spectral data """
+    def __init__(self, data,
+                 wavelengths: np.ndarray=None,
+                 axis=None,
+                 *args,
+                 **kwargs):
+        if wavelengths is None:
+            if isinstance(data, SpectralData) and data.wavelengths is not None:
+                wavelengths = data.wavelengths
+            else:
+                raise TypeError('SpectralData expected wavelength data, but it '
+                                'was not specified.')
         if axis is None:
-            self._axis = get_matching_axis(self._data.shape, wavelengths.size)
+            if isinstance(data, ColorSpaceData):
+                if data.axis is None:
+                    axis = get_matching_axis(data.data.shape, wavelengths.size)
+                else:
+                    axis = data.axis
+            else:
+                axis = get_matching_axis(data.shape, wavelengths.size)
         super().__init__(data, axis, *args, **kwargs)
         self._wavelengths = wavelengths
 
-    def change_white_point(self, illuminant: Illuminant, observer: Observer):
-        self._illuminant = illuminant
-        self._observer = observer
+    @property
+    def wavelengths(self):
+        return self._wavelengths
+
+    def _get_kwargs(self):
+        kwargs = super()._get_kwargs()
+        kwargs['wavelengths'] = self.wavelengths
+        return kwargs
 
 
-class WhitePointSensitive(ColorSpaceDataBase):
+# noinspection PyMethodOverriding
+class WhitePointSensitive(ColorSpaceDataImpl):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @property
-    def illuminant(self):
-        return self._illuminant
-
-    @illuminant.setter
-    def illuminant(self, ill):
+    @ColorSpaceDataImpl.illuminant.setter
+    def illuminant(self, ill: Illuminant):
         self.change_white_point(ill, self._observer)
 
-    @property
-    def observer(self):
-        return self._observer
-
-    @observer.setter
+    @ColorSpaceDataImpl.observer.setter
     def observer(self, obs):
         self.change_white_point(self._illuminant, obs)
 
@@ -227,21 +354,14 @@ class LabData(WhitePointSensitive):
     pass
 
 
+# noinspection PyMethodOverriding
 class RgbsSensitive(WhitePointSensitive):
 
-    @property
-    def rgbs(self):
-        return self._rgbs
-
-    @rgbs.setter
+    @WhitePointSensitive.rgbs.setter
     def rgbs(self, r: RgbSpecification):
         self.change_rgbs(r, self._caa)
 
-    @property
-    def caa(self):
-        return self._caa
-
-    @caa.setter
+    @WhitePointSensitive.caa.setter
     def caa(self, c):
         self.change_rgbs(self._rgbs, c)
 
